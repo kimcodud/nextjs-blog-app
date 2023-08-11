aad82acae2c234bdb30a155843e28e6d862f24cd
김채영 <cccodud@gmail.com>
2023-08-11 16:45:28 +0900

    add: 포스트 데이터 가져와서 보여주기

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,49 @@
+import { getAllPostIds, getSortedPostsData, getPostData } from '@/lib/post';
 import React from 'react';
+import Head from 'next/head';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import homeStyles from '../../styles/Home.module.css';
 
-const Post = () => {
-  return <div>[id]</div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={homeStyles.headingXl}>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: {id: 'pre-rendering}, {params...}}]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
